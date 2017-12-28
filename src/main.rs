a418ebb9685d8d3be7fbc440d5036fe2292d7848
Thomas Gideon <cmdln@thecommandline.net>
2017-12-28 12:57:03 -0500

    Get both ports listening at the same time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate empholite;
+#[macro_use]
+extern crate error_chain;
 extern crate dotenv;
 extern crate env_logger;
 extern crate iron;
@@ -9,15 +11,18 @@ extern crate mount;
 extern crate router;
 extern crate staticfile;
 
-use empholite::{replay_response, IndexHandler, Result, ResultExt};
 use dotenv::dotenv;
 use iron::prelude::*;
 use logger::Logger;
 use mount::Mount;
 use router::Router;
 use staticfile::Static;
+
 use std::env;
 use std::path::Path;
+use std::thread;
+
+use empholite::{replay_response, IndexHandler, Result, ResultExt};
 
 fn main() {
     match bootstrap() {
@@ -43,11 +48,6 @@ fn bootstrap() -> Result<()> {
 }
 
 fn bootstrap_config() -> Result<()> {
-    let host: String = env::var("EMPHOLITE_CONFIG_HOST").unwrap_or("0.0.0.0".into());
-    let port: i32 = env::var("EMPHOLITE_CONFIG_PORT")
-        .unwrap_or("8080".into())
-        .parse()
-        .chain_err(|| "Could not parse port as a number.")?;
     let client_path = env::var("EMPHOLITE_CLIENT_PATH").unwrap_or("./target/client".to_owned());
 
     let mut router = Router::new();
@@ -67,10 +67,28 @@ fn bootstrap_config() -> Result<()> {
     chain.link_before(logger_before);
     chain.link_after(logger_after);
 
+    // iron's listener blocks the current thread so spin it up in its own thread or else we cannot
+    // set up the replay response thread
+    let handler: thread::JoinHandle<Result<()>> = thread::spawn(|| {
+        let host: String = env::var("EMPHOLITE_CONFIG_HOST").unwrap_or("0.0.0.0".into());
+        let port: i32 = env::var("EMPHOLITE_CONFIG_PORT")
+            .unwrap_or("8080".into())
+            .parse()
+            .chain_err(|| "Could not parse port as a number.")?;
+        let address: &str = &format!("{}:{}", host, port);
+        Iron::new(chain)
+            .http(address)
+            .chain_err(move || "Could not start server")?;
+        Ok(())
+    });
+
+    // set up a replay response process
     replay_response();
-    let address: &str = &format!("{}:{}", host, port);
-    Iron::new(chain)
-        .http(address)
-        .chain_err(|| "Could not start server")?;
-    Ok(())
+
+    // on interruption of the replay process, report any error from the web serving thread
+    if let Ok(result) = handler.join() {
+        result
+    } else {
+        bail!("Problem with the main web server thread.")
+    }
 }
