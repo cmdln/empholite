5a47c2f667edc227aabec5ac88e1de193bc0ac2c
Thomas Gideon <cmdln@thecommandline.net>
2020-08-10 15:50:31 -0400

    Remove redundant struct.

--- server/src/handlers/rest.rs
+++ server/src/handlers/rest.rs
@@ -8,32 +8,14 @@ use actix_web::{
     web::{self, Data, Json},
     HttpResponse, Result,
 };
-use chrono::NaiveDateTime;
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use std::convert::TryInto;
-use uuid::Uuid;
-
-// TODO remove after re-factoring Recipe::payload into serde_json::Value
-#[derive(Debug, Serialize, Deserialize)]
-pub(crate) struct Recipe {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub id: Option<Uuid>,
-    pub url: String,
-    pub rules: Vec<shared::Rule>,
-    pub payload: Value,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub created_at: Option<NaiveDateTime>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub updated_at: Option<NaiveDateTime>,
-}
 
 #[actix_web::post("/api/recipe")]
 pub(crate) async fn create_recipe(
     db_pool: Data<DbPool>,
-    recipe: Json<Recipe>,
+    recipe: Json<shared::Recipe>,
 ) -> Result<HttpResponse> {
-    let Recipe {
+    let shared::Recipe {
         url,
         payload,
         rules,
